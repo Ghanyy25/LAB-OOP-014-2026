9e15a6fa6bf5c506531fa5fa835bcb707d5da36d
SEKENSSE <gamalborongmanguma@gmail.com>
2026-04-29 15:43:48 +0800

    Tugas Lab 5

---
+++ H071251036/Tugas Lab 5/soal 2/interaksiinternet.java
@@ -0,0 +1,7 @@
+interface InteraksiInternet {
+    void hubungkanWiFi();
+}
+
+interface KontrolSuara {
+    void prosesPerintah(String perintah);
+}
---
+++ H071251036/Tugas Lab 5/soal 2/smartspeaker.java
@@ -0,0 +1,21 @@
+class SmartSpeaker extends PerangkatElektronik implements InteraksiInternet, KontrolSuara {
+
+    public SmartSpeaker(String merk, int dayaListrik) {
+        super(merk, dayaListrik);
+    }
+
+    @Override
+    public void cekFungsi() {
+        System.out.println("Speaker pintar siap digunakan.");
+    }
+
+    @Override
+    public void hubungkanWiFi() {
+        System.out.println("Terhubung ke WiFi.");
+    }
+
+    @Override
+    public void prosesPerintah(String perintah) {
+        System.out.println("Memproses perintah: " + perintah);
+    }
+}
